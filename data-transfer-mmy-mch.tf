d096dc4b20c5b85c4e02a4b11a0ea52f3ac5ca96
gonobobelka <yuda4@yandex.ru>
2025-08-08 17:32:44 +0300

    YCDOCIO-4928: update TF

--- data-transfer-mmy-mch.tf
+++ data-transfer-mmy-mch.tf
@@ -12,6 +12,7 @@ locals {
   source_password      = ""    # Set the source cluster password.
 
   # Target database settings:
+  target_db_name  = "" # Set the target cluster database name.
   target_user     = "" # Set the username for ClickHouse database.
   target_password = "" # Set the user password for ClickHouse database.
 
@@ -40,10 +41,10 @@ resource "yandex_vpc_subnet" "subnet-b" {
   v4_cidr_blocks = ["10.2.0.0/16"]
 }
 
-resource "yandex_vpc_subnet" "subnet-c" {
-  description    = "Subnet in the ru-central1-c availability zone"
-  name           = "subnet-c"
-  zone           = "ru-central1-c"
+resource "yandex_vpc_subnet" "subnet-d" {
+  description    = "Subnet in the ru-central1-d availability zone"
+  name           = "subnet-d"
+  zone           = "ru-central1-d"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.3.0.0/16"]
 }
@@ -171,20 +172,28 @@ resource "yandex_mdb_clickhouse_cluster" "clickhouse-cluster" {
 
   host {
     type      = "ZOOKEEPER"
-    zone      = "ru-central1-c"
-    subnet_id = yandex_vpc_subnet.subnet-c.id
+    zone      = "ru-central1-d"
+    subnet_id = yandex_vpc_subnet.subnet-d.id
   }
 
-  database {
-    name = local.source_db_name
+  lifecycle {
+    ignore_changes = [database, user]
   }
+}
 
-  user {
-    name     = local.target_user
-    password = local.target_password
-    permission {
-      database_name = local.source_db_name
-    }
+resource "yandex_mdb_clickhouse_database" "target-db" {
+  cluster_id = yandex_mdb_clickhouse_cluster.clickhouse-cluster.id
+  name       = local.target_db_name
+}
+
+resource "yandex_mdb_clickhouse_user" "target-user" {
+  cluster_id = yandex_mdb_clickhouse_cluster.clickhouse-cluster.id
+  name       = local.target_user
+  password   = local.target_password
+  permission {
+    database_name = yandex_mdb_clickhouse_database.target-db.name
+  }
+  settings {
   }
 }
 
@@ -213,7 +222,7 @@ resource "yandex_datatransfer_endpoint" "mch-target" {
       connection {
         connection_options {
           mdb_cluster_id = yandex_mdb_clickhouse_cluster.clickhouse-cluster.id
-          database       = local.source_db_name
+          database       = local.target_db_name
           user           = local.target_user
           password {
             raw = local.target_password
